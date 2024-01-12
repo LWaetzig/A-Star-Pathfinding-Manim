d20b58255a50aa35a9100163bf814e3577972884
Nicho Link <nicholaselink@gmail.com>
2024-01-12 18:05:35 +0100

    WIP

--- heuristic_scene.py
+++ heuristic_scene.py
@@ -9,9 +9,9 @@ class HeuristicScene(MovingCameraScene):
         point_s = Dot(color=GREEN, radius=0.5).move_to(5 * LEFT)
         point_x = Dot(color=BLUE, radius=0.5)
         point_e = Dot(color=RED, radius=0.5).move_to(5 * RIGHT)
-        letter_s = Text("Start", font_size=30).move_to(point_s.get_center())
+        letter_s = Text("Start", font_size=25).move_to(point_s.get_center())
         letter_x = MathTex("x", font_size=60).move_to(point_x.get_center())
-        letter_e = Text("End", font_size=30).move_to(point_e.get_center())
+        letter_e = Text("End", font_size=25).move_to(point_e.get_center())
         line1 = Line(point_s.get_center(), point_x.get_center(), buff=0.5)
         line2 = Line(point_x.get_center(), point_e.get_center(), buff=0.5)
         linegx = Line(point_s.get_center(), point_x.get_center(), buff=0.5, color=GREEN)
@@ -26,27 +26,27 @@ class HeuristicScene(MovingCameraScene):
 
         formula = MathTex("f(x) = g(x) + h(x)", font_size=90)
 
-        g_X = MathTex("g(x)", color=GREEN, font_size=45).move_to(
+        g_x = MathTex("g(x)", color=GREEN, font_size=45).move_to(
             line1.get_center() + 0.75 * DOWN
         )
-        h_X = MathTex("h(x)", color=RED, font_size=45).move_to(
+        h_x = MathTex("h(x)", color=RED, font_size=45).move_to(
             line2.get_center() + 0.75 * DOWN
         )
 
         directtxt = Text("used to direct towards the goal", font_size=30).move_to(
-            1 * UP
+            0.8333 * UP
         )
         priotxt = Text(
             "A* prioritizes nodes leading towards the goal", font_size=30
-        ).move_to(0.5 * DOWN)
+        ).move_to(0.8334 * DOWN)
         reducetxt = Text(
             "reduces unnecessary examination of the rest of the graph", font_size=30
-        ).move_to(2 * DOWN)
+        ).move_to(2.5 * DOWN)
 
         # Create other stuff
-        arrow1 = Arrow(2.4 * UP, 1 * UP, buff=0.35)
-        arrow2 = Arrow(1 * UP, 0.5 * DOWN , buff=0.35)
-        arrow3 = Arrow(0.5 * DOWN, 2 * DOWN, buff=0.35)
+        arrow1 = Arrow(2.4 * UP, 0.8333 * UP, buff=0.35)
+        arrow2 = Arrow(0.8333 * UP, 0.8334 * DOWN , buff=0.35)
+        arrow3 = Arrow(0.8334 * DOWN, 2.5 * DOWN, buff=0.35)
 
         ###############################
         # Start with the video timeline
@@ -63,21 +63,21 @@ class HeuristicScene(MovingCameraScene):
         self.play(Create(graph), run_time=2)
         self.wait(1)
         # 11
-        self.play(Write(g_X), run_time=1)
+        self.play(Write(g_x), run_time=1)
         self.play(Create(linegx), run_time=3)
         self.wait(1.5)
         # 16.5
-        self.play(Write(h_X), run_time=1)
+        self.play(Write(h_x), run_time=1)
         self.play(Create(linehx), run_time=3)
         self.wait(1.5)
         # 22
         self.play(
-            FadeOut(g_X, shift=DOWN),
+            FadeOut(g_x, shift=DOWN),
             FadeOut(linegx, shift=DOWN),
             FadeOut(linehx, shift=DOWN),
             FadeOut(formula, shift=DOWN), 
             FadeOut(graph, shift=DOWN),
-            h_X.animate.scale(1.5).move_to(2.5 * UP),
+            h_x.animate.scale(1.5).move_to(2.5 * UP),
             run_time=1,
         )
         self.wait(1)
@@ -93,6 +93,19 @@ class HeuristicScene(MovingCameraScene):
         self.wait(7)
         # 40
 
+        self.play(
+            FadeOut(arrow1),
+            FadeOut(arrow2),
+            FadeOut(arrow3),
+            FadeOut(directtxt),
+            FadeOut(priotxt),
+            FadeOut(reducetxt),
+            FadeOut(h_x),
+            run_time=1,
+        )
+        self.wait(1)
+
+        
 
 
 
